cba34e218fe614b6431a6abeb070b1de38ab2cf9
Edward Dume <edwarddume55@gmail.com>
2024-11-08 13:07:04 +0300

    first commit

---
+++ hotel/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Room(models.Model):
+    ROOM_CATEGORIES = (
+        ('YAC', 'AC'),
+        ('DEL', 'DELUXE'),
+        ('KIN', 'KING'),
+        ('QUE', 'QUEEN'),
+    )
+    number = models.IntegerField()
+    category = models.CharField(max_length=3, choices=ROOM_CATEGORIES)
+    beds = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.number},{self.category} with {self.beds} beds'
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+
+    def __str__(self):
+        return f' {self.user} has booked {self.room} from {self.check_in} to {self.check_out}'
