36eb1730ecbfadd937c920c4306c3c508254321e
Edward Dume <edwarddume55@gmail.com>
2024-11-08 16:33:07 +0300

    implementing templates for room booking

--- hotel/forms.py
+++ hotel/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
 
-ROOM_CATEGORIES =  (
-        ('YAC', 'AC'),
-        ('DEL', 'DELUXE'),
-        ('KIN', 'KING'),
-        ('QUE', 'QUEEN'),
-    )
+# ROOM_CATEGORIES =  (
+#         ('YAC', 'AC'),
+#         ('DEL', 'DELUXE'),
+#         ('KIN', 'KING'),
+#         ('QUE', 'QUEEN'),
+#     )
 class AvailabilityForm(forms.Form):
-    room_category = forms.ChoiceField(choices=ROOM_CATEGORIES, required=True)
+    # room_category = forms.ChoiceField(choices=ROOM_CATEGORIES, required=True)
     check_in = forms.DateTimeField(required=True, input_formats=["%Y-%m-%dT%H:%M",])
     check_out = forms.DateTimeField(required=True, input_formats=["%Y-%m-%dT%H:%M",])
 
